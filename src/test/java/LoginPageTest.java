f8887370f4219aaa497943a2f851cfbb33b0a43a
Serg <ksergie@gmail.com>
2018-04-03 15:52:50 +0300

    video has a name

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,5 +1,6 @@
 import com.automation.remarks.junit5.Video;
 import io.github.bonigarcia.SeleniumExtension;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
@@ -11,24 +12,24 @@ import static java.lang.System.setProperty;
 @DisplayName("Login page test")
 public class LoginPageTest {
 
-    static private MainPage mainPage;
-    static private LoginPage loginPage;
+    private MainPage mainPage;
+    private LoginPage loginPage;
     private OverviewPage overviewPage;
 
     @BeforeAll
-    static void setup(WebDriver driver) {
+    static void setUp(WebDriver driver) {
         setProperty("wdm.edgeVersion", "3.14393");
-        mainPage = new MainPage(driver);
-        loginPage = new LoginPage(driver);
-        driver.manage().window().maximize();
-        driver.get("https://exitget.com");
-        mainPage.clickHeaderLoginButton();
     }
 
     @TestTemplate
     @Video(name = "Login page. Login with correct data")
     @DisplayName("Login page. Login with correct data")
     void testLoginWithCorrectData(WebDriver driver){
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("exitgetest@gmail.com", "20exitget17");
         overviewPage = new OverviewPage(driver);
         Assertions.assertEquals("VISITATIONS", overviewPage.getHeader(), "FAULT - We are not on the Overview page");
@@ -38,6 +39,11 @@ public class LoginPageTest {
     @Video(name = "Login page. Login without data")
     @DisplayName("Login page. Login without data")
     void testLoginWithoutData(WebDriver driver) {
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("", "");
         Assertions.assertEquals("You must provide the email address you registered with.", loginPage.getTooltip(), "FAULT - Tooltip is not correspond the user's input");
     }
@@ -46,6 +52,11 @@ public class LoginPageTest {
     @Video(name = "Login page. Login without password")
     @DisplayName("Login page. Login without password")
     void loginWithoutPasswd(WebDriver driver) {
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("exitgetest@gmail.com", "");
         Assertions.assertEquals("You must provide a password to login.", loginPage.getTooltip(), "FAULT - Tooltip is not correspond the user's input");
     }
@@ -54,6 +65,11 @@ public class LoginPageTest {
     @Video(name = "Login page. Login with incorrect email")
     @DisplayName("Login page. Login with incorrect email")
     void loginWithIncorrectEmail(WebDriver driver){
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("123qwer", "20exitget17");
         Assertions.assertEquals("You must provide a valid email address.", loginPage.getTooltip(), "FAULT - Tooltip is not correspond the user's input");
     }
@@ -62,6 +78,11 @@ public class LoginPageTest {
     @Video(name = "Login page. Login with correct email and incorrect password")
     @DisplayName("Login page. Login with correct email and incorrect password")
     void loginWithCorrectEmailIncorrectPasswd(WebDriver driver){
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("exitgetest@gmail.com", "20exitget20");
         Assertions.assertEquals("The login information you provided was not correct.", loginPage.getTooltip(), "FAULT - Tooltip is not correspond the user's input");
     }
@@ -70,6 +91,11 @@ public class LoginPageTest {
     @Video(name = "Login page. Login with unregistered email")
     @DisplayName("Login page. Login with unregistered email")
     void loginWithUnregisteredEmail(WebDriver driver){
+        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.get("https://exitget.com");
+        mainPage.clickHeaderLoginButton();
         loginPage.login("exitgetest999999@gmail.com", "20exitget17");
         Assertions.assertEquals("The login information you provided was not correct.", loginPage.getTooltip(), "FAULT - Tooltip is not correspond the user's input");
     }
